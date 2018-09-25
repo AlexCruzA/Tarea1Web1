0ccd51d180568acbba84dc1439fbcab56e84284c
Alex <alexander09cruz@hotmail.com>
2018-09-24 21:05:18 -0600

    FORM creation successfuly

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	<div>
+		welcome <?php echo $_POST["name"], " ", $_POST["lastname"]; ?><br>
+		Your email address is: <?php echo $_POST["email"]; ?>
+	</div>
+</body>
+</html>
