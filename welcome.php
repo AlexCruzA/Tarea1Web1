b93f0a1474155c1bcb26fb19a35517a3f98bcf14
Alex <alexander09cruz@hotmail.com>
2018-09-24 21:22:53 -0600

    Code has been complete and some design added.

--- welcome.php
+++ welcome.php
@@ -2,9 +2,11 @@
 <html>
 
 <body>
-	<div>
-		welcome <?php echo $_POST["name"], " ", $_POST["lastname"]; ?><br>
-		Your email address is: <?php echo $_POST["email"]; ?>
-	</div>
+	<center>
+		<div style="background-color:powderblue;">
+		<h1> Welcome </h1> <h1 style="color:blue;"><?php echo $_POST["name"], " ", $_POST["lastname"]; ?></h1><br>
+		<h1> Your email address is:</h1> <h1 style="color:blue;"><?php echo $_POST["email"]; ?></h1>
+		</div>
+	</center>
 </body>
 </html>
